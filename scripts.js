3ed996bb8a02ab08048b8f8d886f3ec331be225e
Clarence Martin <marticla000@warren.k12.in.us>
2025-01-15 12:16:46 -0500

    blahblah

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ book = {
     title: "Jurassic Park",
     author: "Michael Crichton",
     year: "1990",
-    summary: "Jurassic Park is a 1990 science fiction novel written by Michael Crichton; it is a cautionary tale about genetic engineering that presents the collapse of a zoological park which showcases genetically recreated dinosaurs to illustrate the mathematical concept of chaos theory and its real-world implications.",
     getSummary: function(){
         return `Title: ${this.title}, Author: ${this.author}`;
     }
